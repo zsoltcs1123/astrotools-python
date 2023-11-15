364a467f850918f1613d39e91ca3bb9b781decc5
cszscc <zsolt.csikos@codecool.com>
2023-11-15 12:14:55 +0100

    generate new timeline

--- src/app/main_new.py
+++ src/app/main_new.py
@@ -5,14 +5,14 @@ from tools.timeline_config import TimelineConfig
 
 
 def timeline():
-    start = datetime(2023, 11, 4)
-    end = datetime(2023, 11, 13)
+    start = datetime(2023, 11, 13)
+    end = datetime(2023, 12, 2)
 
     timeline_config = TimelineConfig.default_no_moon(start, end)
     timeline = Timeline(timeline_config)
     timeline_printer = TimelinePrinter(timeline)
 
-    timeline_printer.print_to_file("timeline_nov_4_nov_12_no_moon")
+    timeline_printer.print_to_file("timeline_nov_13_dec_2_no_moon")
 
 
 if __name__ == "__main__":
