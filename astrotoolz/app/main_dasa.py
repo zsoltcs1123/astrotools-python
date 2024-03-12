0e7ef0810ad8f93e1b991faf2785b978dc2a02c5
zsoltcs <cszs1123@gmail.com>
2024-03-12 22:59:38 +0100

    fix dasa main

--- astrotoolz/app/main_dasa.py
+++ astrotoolz/app/main_dasa.py
@@ -3,17 +3,20 @@ from datetime import datetime
 import pytz
 import requests
 
-from astrotoolz.core.objects.points import MOON
-from astrotoolz.core.positions.position_factory import create_geo_position
-from astrotoolz.core.zodiac.positions.mapped_geo_position import MappedGeoPosition
+from astrotoolz.core.enums import NodeCalc, Zodiac
+from astrotoolz.core.points import MOON
+from astrotoolz.core.positions.factory.geo_factory import GeoFactory
+from astrotoolz.core.zodiac.mapper.position_mapper import PositionMapper
 from astrotoolz.out.file import to_text_file
-from astrotoolz.tools.dasa.dasa import Dasa, DasaLevel
+from astrotoolz.tools.dasa.dasa import DasaLevel
 from astrotoolz.tools.dasa.dasa_factory import generate_dasas
 from astrotoolz.tools.dasa.dasa_printer import print_dasas
 
 
 def get_coin_dasa(symbol: str):
-    response = requests.get(f"http://localhost:8001/birthdate?symbol={symbol}")
+    response = requests.get(
+        "http://localhost:8001/birthdate?contract_address=0x6c6ee5e31d828de241282b9606c8e98ea48526e2"
+    )
     birth_date = response.json()["birth_date"]
 
     if birth_date is None:
@@ -23,8 +26,11 @@ def get_coin_dasa(symbol: str):
         pytz.utc
     )
 
-    moon_position = create_geo_position(MOON, birth_date)
-    moon_mapped = MappedGeoPosition(moon_position)
+    position_factory = GeoFactory(NodeCalc.MEAN)
+    position_mapper = PositionMapper()
+    moon_position = position_factory.create_position(MOON, birth_date)
+    moon_mapped = position_mapper.map_position(moon_position, [Zodiac.SIDEREAL])
+
     res = generate_dasas(moon_mapped, DasaLevel.Pratyantar)
 
     current_date = datetime.now().astimezone(pytz.utc)
@@ -41,7 +47,7 @@ def get_coin_dasa(symbol: str):
     dasa_str += f"Coin: {symbol}\n"
     dasa_str += f"Birth date: {birth_date}\n"
     dasa_str += f"Moon longitude: {moon_mapped.vedic.lon}\n"
-    dasa_str += f"Moon position: {moon_mapped.vedic.position}\n"
+    dasa_str += f"Moon position: {moon_mapped.vedic.zodiacal_position}\n"
     dasa_str += f"Moon Nakshatra: {moon_mapped.vedic.nakshatra.name}\n"
     dasa_str += f"Moon Nakshatra ruler: {moon_mapped.vedic.nakshatra.ruler}\n"
     dasa_str += f"Current Maha dasa: {current_maha_dasa}\n"
@@ -53,15 +59,5 @@ def get_coin_dasa(symbol: str):
 
 
 if __name__ == "__main__":
-    for symbol in [
-        "aix",
-        "cre",
-        "hot",
-        "olas",
-        "unibot",
-        "xgpt",
-        "wik",
-        "trendx",
-    ]:
-        print(f"Getting dasa for {symbol}")
-        get_coin_dasa(symbol)
+    print("Getting dasa for HOT")
+    get_coin_dasa("HOT")
